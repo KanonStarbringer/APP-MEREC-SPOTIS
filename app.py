bbea92d7a00089e540d84ccb02c78083bb730cda
Tullio Pires <94844118+KanonStarbringer@users.noreply.github.com>
2024-09-28 07:18:42 -0300

    app.py

--- app.py
+++ app.py
@@ -527,8 +527,9 @@ def normalize_spotis(matrix, ideal_solution, criteria_ranges, criterion_types):
             s_j_star = float(ideal_solution[j])
             s_j_max = float(criteria_ranges[f"C{j+1}"]["max"])
             s_j_min = float(criteria_ranges[f"C{j+1}"]["min"])
-            
-            normalized_value = (s_ij - s_j_min) / (s_j_max - s_j_min)
+            dif = float(abs(s_ij-s_j_star))
+
+            normalized_value = (dif - s_j_min) / (s_j_max - s_j_min)
             
             normalized_spotis.iloc[i, j+1] = normalized_value
     
